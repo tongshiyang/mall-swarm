5ab7fd76a44de8a8c83266a8eb44420e5498c632
tongshiyang <tongshiyang@126.com>
2020-12-22 10:24:37 +0800

    测试

--- mall-auth/src/main/java/com/macro/mall/auth/config/WebSecurityConfig.java
+++ mall-auth/src/main/java/com/macro/mall/auth/config/WebSecurityConfig.java
@@ -18,7 +18,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
